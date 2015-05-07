19ebf45cc78f847bad90d421792a632056df9961
seanmcpe <seanmcpex@gmail.com>
2015-05-07 18:25:37 -0400

    Update Main.php

--- src/Sean_M/Main.php
+++ src/Sean_M/Main.php
@@ -1,1 +1,21 @@
+<?php
+namespace Sean_M\NoDiamonds;
 
+use pocketmine\Player;
+use pocketmine\event\player\PlayerItemHeldEvent;
+
+class Main extends PluginBase implements Listener{
+   
+     public function onEnable(){      
+          $this->getServer()->getPluginManager()->registerEvents($this);
+          $this->getLogger()->info("NoDiamonds enabled!");
+     }
+
+     public function itemHeld(PlayerItemHeldEvent $event){
+          $p = $event->getPlayer();
+               if($p->getName() == "AA12"){
+               if($p->getItemInHand() == 264) $p->kick("Haha!");
+               
+          }
+     }
+}
